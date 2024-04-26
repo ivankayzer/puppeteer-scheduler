d01d2bb2cc64a4a1c7424f6becf4dc97ead0572a
Ivan Kayzer <ivan.kayzer@outlook.com>
2024-04-26 21:01:05 +0200

    fix demo script

--- scripts/aaaauto.ts
+++ scripts/aaaauto.ts
@@ -1,8 +1,8 @@
 import { Page } from "puppeteer";
 
-const { setViewport, wait } = require("../dist/helpers/helpers");
-const chats = require("../dist/helpers/chats");
-const frequency = require("../dist/helpers/frequency");
+import Helpers from "../src/helpers/helpers";
+import chats from "../src/helpers/chats";
+import Frequency from "../src/helpers/frequency";
 
 const url =
   "https://www.aaaauto.pl/benzin/#!&make[]=74&make[]=87&make[]=130&model[]=1207&model[]=36613&model[]=1826&model[]=844&palivo=1&transmission=A&pmax=70000";
@@ -10,27 +10,27 @@ const url =
 const script = async (page: Page) => {
   await page.goto(url);
 
-  await setViewport(page);
+  await Helpers.setViewport(page);
 
-  await wait(5000);
+  await Helpers.wait(5000);
 
   return page.evaluate(async () => {
     return [
       ...document.querySelectorAll("#carsGrid .card.box"),
     ]
       .filter(n => n.querySelector(".carInfoImg"))
-      .map(function (node) {
-        const href = node.querySelector('a').href;
+      .map(function (node: Element) {
+        const href = node.querySelector('a')!.href;
 
         return {
           id: href.split('#')[0],
-          title: node.querySelector('h2 > a').innerText,
-          price: node.querySelector(".carPrice h3.notranslate:not(.primary):not(.error)").innerText,
+          title: node.querySelector<HTMLElement>('h2 > a')?.innerText,
+          price: node.querySelector<HTMLElement>(".carPrice h3.notranslate:not(.primary):not(.error)")?.innerText,
           specs: node
-            .querySelector(".columnsTags")
-            .innerText.split("\n").join(', '),
+            .querySelector<HTMLElement>(".columnsTags")
+            ?.innerText.split("\n").join(', '),
           img:
-            "<a href='" + node.querySelector(".carInfoImg > figure > img").src + "'>Img</a>",
+            "<a href='" + node.querySelector<HTMLImageElement>(".carInfoImg > figure > img")?.src + "'>Img</a>",
           url:
             "<a href='" + href + "'>Link</a>",
         };
@@ -41,6 +41,6 @@ const script = async (page: Page) => {
 module.exports = {
   script,
   name: "AAA Auto",
-  chatId: chats.IVAN_KAYZER,
-  frequency: frequency.hours(2),
+  chatId: chats.DEFAULT,
+  frequency: Frequency.hours(2),
 };
